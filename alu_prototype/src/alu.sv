08ab40728e22bc8d7643c4eef6b7278795f724ef
Tanish <tanishgheewala@gmail.com>
2026-03-22 16:54:45 -0700

    test

--- alu_prototype/src/alu.sv
+++ alu_prototype/src/alu.sv
@@ -1,15 +1,17 @@
-/*
-* alu module executes all logical and arithmetic instructions.
-* Using alu_interfacce to define ports. Essentailly a mux that
-* outputs the correct line for the operation being done.
-*/
+// alu.sv
+
+`timescale 1ns/1ps
+
 module alu(
     alu_interface.dut alu_if
 );
     always_comb begin
         case(alu_if.alu_opcode)
+        
             //define cases here
             //EX: 6'b011001: alu_if.out_data = alu_if.in_data_0 + alu_if.in_data_1;
+
+
         endcase
     end
 endmodule
