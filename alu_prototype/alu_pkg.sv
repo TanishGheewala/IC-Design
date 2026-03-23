1c107048417e5e5bcc0e61ae1c20a3f3e4cd81ff
Tanish <tanishgheewala@gmail.com>
2026-03-22 17:15:53 -0700

    Created alu_pkg.sv

---
+++ alu_prototype/alu_pkg.sv
@@ -0,0 +1,14 @@
+package ALU_pkg;
+
+import uvm_pkg::*;
+`include "alu_driver.sv"
+`include "alu_monitor.sv"
+`include "alu_sequence_item.sv"
+`include "alu_sequence.sv"
+`include "alu_scoreboard.sv"
+`include "alu_agent.sv"
+`include "alu_environment.sv"
+`include "alu_test.sv"
+`include "top.sv"
+
+endpackage
