a0486027431db9f49b6c11ea8f2f9b05b3ed1081
Tanish <tanishgheewala@gmail.com>
2026-03-23 14:03:39 -0700

    Op-Casting

--- alu_prototype/src/alu.sv
+++ alu_prototype/src/alu.sv
@@ -1,24 +1,43 @@
 // alu.sv
 `timescale 1ns/1ps
 
-module alu(alu_interface.dut alu_if);
-//Move some of this out to alu_interface later
-    localparam int WIDTH = $bits()//;
+module alu(alu_interface.alu_dut alu_if);
+    localparam int WIDTH = $bits(alu_if.in_data_0);
+
     // signed/unsigned
     logic [WIDTH-1:0]        a_u, b_u;
     logic signed [WIDTH-1:0] a_s, b_s;
     logic [WIDTH-1:0]        result;
-    logic [WIDTH-1:0]        result_s;
 
-    // op casting
+    // Op-Casting
+    always_comb begin
+    a_u = alu_if.in_data_0;
+    b_u = alu_if.in_data_1;
+    a_s = $signed(alu_if.in_data_0);
+    b_s = $signed(alu_if.in_data_1);
+    end
 
     // combinational logic
     always_comb begin
         result = '0; //default result = '0;
         unique case(alu_if.alu_opcode)
+        /*
+        ADD
+        SUB
 
-            //define cases here
-            //EX: 6'b011001: alu_if.out_data = alu_if.in_data_0 + alu_if.in_data_1;
+        AND
+        OR
+        XOR
+
+        SLL
+        SRL
+        SRA
+
+        SLT
+        SLTU
+
+        PASSTHROUGH
+        */
 
         default: result = '0;
         endcase
