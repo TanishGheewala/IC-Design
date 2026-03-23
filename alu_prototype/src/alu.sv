c9276ecf3183f46bc0f537d102e5d834acffa9ba
Tanish <tanishgheewala@gmail.com>
2026-03-23 14:08:39 -0700

    Combinational Logic

--- alu_prototype/src/alu.sv
+++ alu_prototype/src/alu.sv
@@ -1,10 +1,11 @@
 // alu.sv
+
 `timescale 1ns/1ps
 
 module alu(alu_interface.alu_dut alu_if);
     localparam int WIDTH = $bits(alu_if.in_data_0);
 
-    // signed/unsigned
+    // Signed/Unsigned
     logic [WIDTH-1:0]        a_u, b_u;
     logic signed [WIDTH-1:0] a_s, b_s;
     logic [WIDTH-1:0]        result;
@@ -17,27 +18,32 @@ module alu(alu_interface.alu_dut alu_if);
     b_s = $signed(alu_if.in_data_1);
     end
 
-    // combinational logic
+    // Combinational Logic
     always_comb begin
-        result = '0; //default result = '0;
+        result = '0; // default result = '0;
         unique case(alu_if.alu_opcode)
-        /*
-        ADD
-        SUB
 
-        AND
-        OR
-        XOR
+        // Arithmetic
+        6'b011001: result = a_u + b_u; // ADD
+        6'b011010: result = a_u - b_u; // SUB
 
-        SLL
-        SRL
-        SRA
+        // Bitwise Logic
+        6'b011011: result = a_u & b_u; // AND
+        6'b011100: result = a_u | b_u; // OR    
+        6'b011101: result = a_u ^ b_u; // XOR
 
-        SLT
-        SLTU
+        // Shifts
+        6'b011110: result = a_u << b_u[4:0]; // SLL
+        6'b011111: result = a_u >> b_u[4:0]; // SRL
+        6'b100000: result = a_s >>> b_u[4:0]; // SRA
 
-        PASSTHROUGH
-        */
+        // Comparisons
+        6'b100001: result = (a_s < b_s) ? 32'd1 : 32'd0; // SLT
+        6'b100010: result = (a_u < b_u) ? 32'd1 : 32'd0; // SLTU
+
+        // Passthrough
+        6'b100011: result = a_u; // Pass A
+        6'b100100: result = b_u; // Pass B
 
         default: result = '0;
         endcase
