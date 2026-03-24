3081a855676e2747961284f1f9973cfae6137908
Housain Alsafar <housainalsafar@icloud.com>
2026-03-24 01:36:08 -0700

    Update alu_scoreboard.sv

--- alu_prototype/alu_uvm_classes/alu_scoreboard.sv
+++ alu_prototype/alu_uvm_classes/alu_scoreboard.sv
@@ -8,12 +8,41 @@ class alu_scoreboard extends uvm_scoreboard;
     uvm_analysis_imp #(alu_item, alu_scoreboard) ap_imp;
 
     function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
         ap_imp = new("ap_imp", this);
     endfunction
 
     virtual function void write(alu_item data);
         bit [31:0] expected;
 
-        // ALU expected-value logic goes here
+        case (data.alu_op)
+
+            6'b011001: expected = data.in_data_0 + data.in_data_1;
+            6'b011010: expected = data.in_data_0 - data.in_data_1;
+
+            6'b011011: expected = data.in_data_0 & data.in_data_1;
+            6'b011100: expected = data.in_data_0 | data.in_data_1;
+            6'b011101: expected = data.in_data_0 ^ data.in_data_1;
+
+            6'b011110: expected = data.in_data_0 << data.in_data_1[4:0];
+            6'b011111: expected = data.in_data_0 >> data.in_data_1[4:0];
+            6'b100000: expected = $signed(data.in_data_0) >>> data.in_data_1[4:0];
+
+            6'b100001: expected = ($signed(data.in_data_0) < $signed(data.in_data_1)) ? 32'd1 : 32'd0;
+            6'b100010: expected = (data.in_data_0 < data.in_data_1) ? 32'd1 : 32'd0;
+
+            6'b100011: expected = data.in_data_0;
+            6'b100100: expected = data.in_data_1;
+
+            default: expected = 32'd0;
+
+        endcase
+
+        if (expected == data.out_data)
+            `uvm_info("ALU_SB", "PASS", UVM_LOW)
+        else
+            `uvm_error("ALU_SB", "FAIL")
+
     endfunction
+
 endclass
